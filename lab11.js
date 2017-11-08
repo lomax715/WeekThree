0ab9b9f28e3b8ac3069d80738b906fe582aaec41
Jack <lomax15@me.com>
2017-11-08 10:09:21 -0800

    made everything a note because of merge issues

--- lab11.js
+++ lab11.js
@@ -1,13 +1,13 @@
-'use strict';
+// 'use strict';
 
-//let numArray = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19'];
+// //let numArray = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19'];
 
-// funtion getRandom(numArray) {
-//     return Math.random();
+// // funtion getRandom(numArray) {
+// //     return Math.random();
+// // }
+
+// function myFunction() {
+//     var x = Math.floor((Math.random() * 10) + 1);
 // }
 
-function myFunction() {
-    var x = Math.floor((Math.random() * 10) + 1);
-}
-
-// adding notes so i can push correctly
+// // adding notes so i can push correctly
